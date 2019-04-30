9b22ab208f4144ac909bf9332dcd67ca51ef4679
kieranmachale <c16777089@mydit.ie>
2019-04-30 18:54:48 +0100

    Creating Star class

---
+++ java/src/ie/tudublin/Star.java
@@ -0,0 +1,18 @@
+package ie.tudublin;
+import processing.core.PApplet;
+
+//Creating Star class
+public class Star extends PApplet{
+    
+    float posx;
+    float posy;
+    float posz;
+
+    //Star constructor
+    public Star(){
+        posx = random(0, width);
+        posy = random(0, height);
+        posz = width;
+    }
+
+}
